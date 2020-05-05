13aa4a73c49df21dd1aff401902820a955d79955
Dany Koudja <koudjadany@gmail.com>
2020-05-05 17:21:31 +0200

    Initial commit

---
+++ src/main/java/com/ddf/pushnotificationserver/PushNotificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.ddf.pushnotificationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushNotificationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddf/pushnotificationserver/PushNotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddf.pushnotificationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
